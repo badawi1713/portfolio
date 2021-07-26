63fe9256bd2d18ac1d7d59faeeb83ab6c0e2bfc1
badawi1713 <dzaky.badawi@gmail.com>
2021-07-26 19:31:00 +0700

    build portfolio with next.js

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Footer from './Footer'
-import Header from './Header'
-import Navbar from './Navbar'
-import Main from './Main'
-import Modal from './Modal'
-import Section from './Section'
-
-export { Footer, Header, Main, Modal, Navbar, Section }
